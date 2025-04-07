7635fc8fc9bc2d93179b210bf58c0fd50c453a85
dillrofd <148703484+dillrofd@users.noreply.github.com>
2025-04-07 20:19:53 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+//1
+let text1 = " Поле 1";
+let tetx2 = " Поле 2";
+if ( text1 !== "" && tetx2 !== "") {
+    console.log ("Обидва поля заповнені");
+} 
+else {
+    console.log( "Не всі поля заповнені");
+}
+
+
+//2
+let  number1 = 7;
+let number2 = 4;
+let together = number1 + number2;
+
+if ( together > 10 ) {
+    console.log ( " Сума більша за 10 ");
+}
+else {
+    console.log ( "Сума менша або дорівнює 10");
+}
+
+
+
+//3
+let textSaved = "I learning ";
+if (textSaved.includes("JavaScript")) {
+    console.log (  "Текст містить слово JavaScript"); 
+}
+else {
+    console.log(  "Текст не містить слово JavaScript");
+}
+
+
+
+//4
+let anyNumer = 13;
+if (anyNumer > 10 && anyNumer < 20 ){
+    console.log ( "Число входить в діапазон від 10 до 20");
+}
+else {
+    console.log (  "Число не входить в діапазон від 10 до 20");
+}
+
+//5
+
+let name = " Jungkook";
+let email = "jeon.jungkook@gmail.com";
+let password = 123456756;
+let nameValid = name.lenght >= 3 ;
+let emailValid = email.includes("@") && email.includes (".") && email.indexOf( "@") && email.lastIndexOf(".");
+let passwordValid = password.lenght >= 6;
+
+if ( nameValid && emailValid && passwordValid ) {
+    console.log( "Перенаправлення на іншу сторінку");
+}
+else {
+    console.log(  "Помилка: неправильне заповнення");
+}
+
